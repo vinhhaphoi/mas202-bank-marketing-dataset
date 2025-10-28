3233ab7c397533116cc8a97db380f9a70ae13572
vinhhaphoi <vinhhaphoi@gmail.com>
2025-10-28 16:27:03 +0700

    update

--- install_run.sh
+++ install_run.sh
@@ -1,17 +1,14 @@
-# 1. Navigate to project directory
-cd /Users/vinhhaphoi/Documents/mas202
-
-# 2. Create virtual environment
+# 1. Create virtual environment
 python3 -m venv venv
 
-# 3. Activate virtual environment
+# 2. Activate virtual environment
 source venv/bin/activate
 
-# 4. Upgrade pip
+# 3. Upgrade pip
 pip install --upgrade pip
 
-# 5. Install required packages
+# 4. Install required packages
 pip install -r requirements.txt
 
-# 6. Run program
+# 5. Run program
 python ana.py
